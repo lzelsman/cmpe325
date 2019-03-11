7c0a8ae2e5b6dfb9d302630caca2cfd87a322844
Lianne Zelsman <l.zelsman@gmail.com>
2019-03-10 23:50:11 -0400

    Only print letter on change

--- ASL_one_hot_encoding/runLive.py
+++ ASL_one_hot_encoding/runLive.py
@@ -23,7 +23,7 @@ with tf.Session() as sess:
     accuracy = graph.get_tensor_by_name('accuracy:0')
 
     # Get the image
-
+    count = 0
     while 1:
         cap = cv2.VideoCapture(0)
         ret, img = cap.read()
@@ -34,6 +34,8 @@ with tf.Session() as sess:
             value = (35, 35)
             blurred = cv2.GaussianBlur(grey, value, 0)
             _, thresh1 = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+            img = cv2.resize(img, dsize=None, fx=1.5, fy=1.5)
+            img = cv2.flip(img, 1) # To flip image
             #cv2.imshow('title',thresh1)
             cv2.imshow('frame',img)
             thresh1 = (thresh1 * 1.0) / 255
@@ -44,8 +46,35 @@ with tf.Session() as sess:
             else:
                 testImage = np.reshape(thresh1, [-1, par.image_size, par.image_size, 3])
             testImage = testImage.astype(np.float32)
-            testY = sess.run(prediction, feed_dict={X: testImage, keep_prob: 1.0})
+            if count == 0: # First iteration
+                testY = sess.run(prediction, feed_dict={X: testImage, keep_prob: 1.0})
+                testY_previous = testY
+            else: 
+                testY_previous = testY
+                testY = sess.run(prediction, feed_dict={X: testImage, keep_prob: 1.0})
+            count += 1
             print(testY)
+            # Print predicted letter, only if it has changed since the last prediction
+            for i in range(len(testY[0])):
+                if testY[0][i] != testY_previous[0][i]:
+                    if testY[0][0] == [1]:
+                        print("A")
+                    elif testY[0][1] == [1]: 
+                        print("B")
+                    elif testY[0][2] == [1]:
+                        print("C")
+                    elif testY[0][3] == [1]:
+                        print("D")
+                    elif testY[0][4] == [1]:
+                        print("G")
+                    elif testY[0][5] == [1]:
+                        print("I")
+                    elif testY[0][6] == [1]:
+                        print("L")
+                    elif testY[0][7] == [1]:
+                        print("V")
+                    elif testY[0][8] == [1]:
+                        print("Y")
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
